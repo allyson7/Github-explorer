b5ecb74497e18cc03fe281d4ce66a0ec29bd7927
allyson7 <allysonlopes0@gmail.com>
2022-08-06 12:00:20 -0400

    :sparkles: importing github repositories information api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+
+// https://api.github.com/users/allyson7/repos
 
 const repository = {
   name: "unform",
@@ -7,6 +12,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/allyson7/repos").then((response) =>
+      response.json().then((data) => setRepositories(data))
+    );
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
